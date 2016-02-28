0c64c11d1611ca1e8286027c034160e516604c5b
Александр Прендота <aleksandrprendota@macbook-air-aleksandr.local>
2016-02-28 21:26:56 +0300

    Initial Commit

---
+++ The simple calculator swift/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  The simple calculator swift
+//
+//  Created by Александр Прендота on 28.02.16.
+//  Copyright © 2016 Александр Прендота. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func figures(sender: UIButton) {
+        
+        //print("Digital is \(sender.tag)")
+    }
+
+    @IBAction func action(sender: UIButton) {
+       
+        
+    }
+    
+    @IBAction func deciminal(sender: AnyObject) {
+        
+    }
+    
+    
+}
+
