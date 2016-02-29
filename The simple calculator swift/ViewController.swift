9d89be12c9c00c861f702bc4c16995e66157bd08
Александр Прендота <aleksandrprendota@MacBook-Air-Aleksandr.local>
2016-02-29 20:30:41 +0300

    add adctions

--- The simple calculator swift/ViewController.swift
+++ The simple calculator swift/ViewController.swift
@@ -7,33 +7,86 @@
 //
 
 import UIKit
+// the digital
+var x = 0
+// the second digital
+var y = 0
+// the operation
+var act = 0
+// the flag for next number
+var flagForNextDigital = 1
+
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
     }
 
     override func didReceiveMemoryWarning() {
+      
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
     @IBAction func figures(sender: UIButton) {
         
-        //Testing git
+        if flagForNextDigital == 1 {
+            x = 0
+            flagForNextDigital = 0
+        }
+        
+        x = x * 10 + Int(sender.tag)
+        self.result.text = String(x)
+         
     }
+    
+    
+    @IBAction func clear(sender: UIButton) {
+        
+        x = 0
+        y = 0
+        self.result.text = String(x)
+        
+    }
+    
 
     @IBAction func action(sender: UIButton) {
-
+        
+        // all action - buttons have tags, for instance : "/" has tag - 1001.
+        
+        if act != 0 && flagForNextDigital != 1  {
+        
+        switch act {
+                
+                case 1001 :
+                    x = x / y
+                case 1002 :
+                    x = x * y
+                case 1003 :
+                    x = x - y
+                case 1004 :
+                    x = x + y
+                
+            default :  self.result.text = String(x)
+                
+            }
+        }
+        
+        act = sender.tag
+        y = x
+        flagForNextDigital = 1
+        self.result.text = String(x)
         
     }
     
     @IBAction func deciminal(sender: AnyObject) {
         
     }
-    
+
+    @IBOutlet var result: UILabel!
     
 }
 
