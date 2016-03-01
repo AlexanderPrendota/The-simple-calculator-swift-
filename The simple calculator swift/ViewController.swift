ae9483665444d713b117be1fae31973bb95639ab
Александр Прендота <aleksandrprendota@MacBook-Air-Aleksandr.local>
2016-03-01 20:08:11 +0300

    new function

--- The simple calculator swift/ViewController.swift
+++ The simple calculator swift/ViewController.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+
 // the digital
-var x = 0
+var x : Double = 0
 // the second digital
-var y = 0
+var y : Double = 0
 // the operation
 var act = 0
 // the flag for next number
-var flagForNextDigital = 1
-
+var flagForNextDigital = true
+// point for decimal figures
+var point = false
+// for deminal formula
+var power : Double = 1
 
 
 class ViewController: UIViewController {
@@ -30,63 +34,84 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         
     }
+    
+    func printCorrectString() {
+        if String(x).hasSuffix(".0"){
+            self.result.text = " " +  String(Int(x))
+        } else {
+            self.result.text = " " +  String(x)
+        }
+    }
+    
+    @IBOutlet var result: UILabel!
 
     @IBAction func figures(sender: UIButton) {
         
-        if flagForNextDigital == 1 {
+        if flagForNextDigital {
             x = 0
-            flagForNextDigital = 0
+            flagForNextDigital = false
         }
-        
-        x = x * 10 + Int(sender.tag)
-        self.result.text = String(x)
-         
+        if !point {
+            x = x * 10 + Double(sender.tag)
+            printCorrectString()
+        } else {
+            x = x + Double(sender.tag)/pow(10, power)
+            power++
+            printCorrectString()
+        }
     }
     
-    
-    @IBAction func clear(sender: UIButton) {
-        
-        x = 0
-        y = 0
-        self.result.text = String(x)
-        
+    @IBAction func inverse(sender: AnyObject) {
+        x = -x
+        printCorrectString()
     }
-    
 
-    @IBAction func action(sender: UIButton) {
+    @IBAction func action(sender: AnyObject) {
         
         // all action - buttons have tags, for instance : "/" has tag - 1001.
         
-        if act != 0 && flagForNextDigital != 1  {
+        if !flagForNextDigital   {
         
-        switch act {
+            switch act {
                 
                 case 1001 :
-                    x = x / y
+                    x = y / x
                 case 1002 :
-                    x = x * y
+                    x = y * x
                 case 1003 :
-                    x = x - y
+                    x = y - x
                 case 1004 :
-                    x = x + y
-                
-            default :  self.result.text = String(x)
+                    x = y + x
+            default : printCorrectString()
                 
             }
         }
         
         act = sender.tag
         y = x
-        flagForNextDigital = 1
-        self.result.text = String(x)
+        flagForNextDigital = true
+        printCorrectString()
+        power = 1
+        point = false
         
+    }
+    
+    @IBAction func clear(sender: UIButton) {
+        
+        flagForNextDigital = true
+        point = false
+        act = 0
+        power = 1
+        x = 0
+        y = 0
+        printCorrectString()
     }
     
     @IBAction func deciminal(sender: AnyObject) {
         
+        if !point {
+            point = true
+        }
     }
-
-    @IBOutlet var result: UILabel!
-    
 }
 
